46ede7f18f3fe79897b7509fe6317632d7bee6af
raeim <raei.mh@gmail.com>
2021-04-18 14:16:00 +0430

    filled basic model methods by UML

---
+++ src/main/java/model/Cell.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Cell {
+    private Card card;
+    private static Cell selectedCell;
+
+    public Card getCellCard() {
+        return this.card;
+    }
+
+    public void addCardToCell(Card card) {
+        setCard(card);
+    }
+
+    private void setCard(Card card) {
+        this.card = card;
+    }
+
+    public void selectCell() {//select cell either needs entry or must be nonstatic
+        selectedCell = this;
+        setSelectedCell(this);
+    }
+
+    public static void deselectCell() {//better to be same as select cell or rename
+        selectedCell = null;
+    }
+
+    public static void setSelectedCell(Cell selectedCell) {
+        Cell.selectedCell = selectedCell;
+    }
+
+    public static Cell getSelectedCell() {
+        return selectedCell;
+    }
+}
