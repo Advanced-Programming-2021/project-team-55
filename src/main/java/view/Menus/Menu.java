707170434ac0db4e6998ccd5ce574313958e99c2
ParhamBateni <mp.bateni@gmail.com>
2021-04-22 16:44:19 +0430

    LoginMenu.txt and MainMenu&ScoreboardMenu.txt testcase added

--- src/main/java/view/Menus/Menu.java
+++ src/main/java/view/Menus/Menu.java
@@ -22,9 +22,6 @@ abstract public class Menu {
         }catch (IOException e){
             e.printStackTrace();
         }
-//        for(User user:User.getAllUsers()){
-//            System.out.println(user);
-//        }
         while (true) {
             switch (currentMenu) {
                 case LOGIN: {
