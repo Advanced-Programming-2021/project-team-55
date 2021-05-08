f87844ad3718a2c7915163bfbad080886b386943
raeim <raei.mh@gmail.com>
2021-05-08 14:42:55 +0430

    Exploder Dragon

--- src/main/java/model/cards/monsters/ExploderDragon.java
+++ src/main/java/model/cards/monsters/ExploderDragon.java
@@ -1,5 +1,8 @@
 package model.cards.monsters;
 
+import controller.gamephasescontrollers.GameController;
+import model.board.CardStatus;
+import model.board.Cell;
 import model.cards.Monster;
 import model.cards.cardfeaturesenums.CardType;
 import model.cards.cardfeaturesenums.MonsterAttribute;
@@ -12,4 +15,37 @@ public class ExploderDragon extends Monster {
                 , 1000, 1000, 0, 3, MonsterAttribute.EARTH, MonsterType.DRAGON, CardType.EFFECTIVE);
     }
 
+    public static String handleEffect(GameController gameController, Cell attackerCell, Cell attackedCell) {
+        if (isNotExploderDragon(attackerCell, attackedCell)) return "";
+        if (exploderDragonDiesDuringBeingAttacked(gameController, attackerCell, attackedCell)) {
+            removePlayers(gameController, attackerCell, attackedCell);
+            return "Exploder Dragon effect activated:  monster card: \\\"" + attackerCell.getCellCard().getName() + "\\\" removed and no one loses LP. ";
+        } else if (exploderDragonDiesDuringAttack(gameController, attackerCell, attackedCell)) {
+            removePlayers(gameController, attackerCell, attackedCell);
+            return "Exploder Dragon effect activated:  monster card: \\\"" + attackedCell.getCellCard().getName() + "\\\" removed and no one loses LP. ";
+        }
+        return "";
+    }
+
+    private static boolean exploderDragonDiesDuringBeingAttacked(GameController gameController, Cell attackerCell, Cell attackedCell) {
+        return attackedCell.getCellCard().getName().equals("Exploder Dragon") &&
+                gameController.getBattlePhaseController().isAttackerStronger(attackerCell, attackedCell);
+    }
+
+    private static boolean exploderDragonDiesDuringAttack(GameController gameController, Cell attackerCell, Cell attackedCell) {
+        return attackerCell.getCellCard().getName().equals("Exploder Dragon") &&
+                !gameController.getBattlePhaseController().isAttackerStronger(attackerCell, attackedCell) &&
+                !gameController.getBattlePhaseController().isAttackerAndAttackedPowerEqual(attackerCell, attackedCell) &&
+                attackedCell.getCardStatus()== CardStatus.OFFENSIVE_OCCUPIED;
+    }
+
+    private static void removePlayers(GameController gameController, Cell attackerCell, Cell attackedCell) {
+        attackedCell.removeCardFromCell(gameController.getCurrentTurnOpponentPlayer().getGameBoard());
+        attackerCell.removeCardFromCell(gameController.getCurrentTurnPlayer().getGameBoard());
+    }
+
+    public static boolean isNotExploderDragon(Cell attackerCell, Cell attackedCell) {
+        return !attackedCell.getCellCard().getName().equals("Exploder Dragon") &&
+                !attackerCell.getCellCard().getName().equals("Exploder Dragon");
+    }
 }
