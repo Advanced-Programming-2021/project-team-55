c339ecf3d1a7a0ca4ed044f433e0c0698afa88a7
ParhamBateni <mp.bateni@gmail.com>
2021-05-16 18:19:53 +0430

    CoinDice bug fixed

--- src/main/java/model/CoinDice.java
+++ src/main/java/model/CoinDice.java
@@ -1,15 +1,15 @@
 package model;
+import model.cards.trapandspells.MagicCylinder;
 
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CoinDice {
 
     public static int rollDice() {
-        return (ThreadLocalRandom.current().nextInt())%6+1;
+        return Math.abs(ThreadLocalRandom.current().nextInt())%6+1;
     }
     public static int tossCoin() {
-        return (ThreadLocalRandom.current().nextInt())%2+1;
+        return Math.abs(ThreadLocalRandom.current().nextInt())%2+1;
     }
 
 }
