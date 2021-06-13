2a4a241473e072f8ade9230fc5daca266d41b8a3
Abolfazl Ghalandary <ab.ghalandary@outlook.com>
2021-06-13 18:11:32 +0430

    improve game

--- src/main/java/yugioh/controller/menucontroller/GameMenuController.java
+++ src/main/java/yugioh/controller/menucontroller/GameMenuController.java
@@ -2,8 +2,12 @@ package yugioh.controller.menucontroller;
 
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import yugioh.view.Menus.DuelMenu;
+import yugioh.view.gamephases.Duel;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,13 +15,33 @@ import java.util.ResourceBundle;
 public class GameMenuController extends MenuController implements Initializable {
 
     public ImageView hoveredImage;
+    public ScrollPane descriptionContainer;
+    public Label description;
+    public Label atkLabel;
+    public Label defLabel;
+    public Label defValue;
+    public Label atkValue;
+    public Label rivalLP;
+    public ProgressBar rivalLPBar;
+    public Label userLP;
+    public ProgressBar userLPBar;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        updateGameStatusUIs();
     }
 
     public void backClicked() throws Exception {
         new DuelMenu().execute();
     }
+
+    public void updateGameStatusUIs() {
+        int opponentLP = Duel.getGameController().currentTurnOpponentPlayer.getLP();
+        int myLP = Duel.getGameController().currentTurnPlayer.getLP();
+        rivalLP.setText(opponentLP + "");
+        userLP.setText(myLP + "");
+        rivalLPBar.setProgress((double) opponentLP / 8000);
+        userLPBar.setProgress((double) myLP / 8000);
+    }
+
 }
