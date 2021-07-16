f54a02ca09bd702f9ae4269d082f8fd1a39c3ebf
ParhamBateni <mp.bateni@gmail.com>
2021-07-16 15:34:03 +0430

    fix the reply in chatRoom and add Image

--- src/main/java/yugioh/client/controller/menucontroller/ChatRoomController.java
+++ src/main/java/yugioh/client/controller/menucontroller/ChatRoomController.java
@@ -12,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -21,6 +22,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import yugioh.client.model.User;
@@ -34,6 +36,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+
+import static yugioh.client.view.ViewInterface.getCommandMatcher;
 
 public class ChatRoomController  extends MenuController implements Initializable{
    // public TextArea message;
@@ -51,13 +56,15 @@ public class ChatRoomController  extends MenuController implements Initializable
 
     public void sendMessage(Event event) throws Exception {
         if(replyMessage.getText().equals("")) {
-            dataOutputStreamForChat.writeUTF("chat " + User.loggedInUser.getNickname() + ": " + message.getText());
+            dataOutputStreamForChat.writeUTF("chat " +"image: "+User.loggedInUser.getProfileImageString() +
+                    " message: "+ User.loggedInUser.getNickname() + ": " + message.getText());
         }
         else{
             String lastReplyMessage;
             if(replyMessage.getText().contains("\n"))lastReplyMessage=replyMessage.getText().substring(0,replyMessage.getText().indexOf("\n"));
             else lastReplyMessage=replyMessage.getText();
-            dataOutputStreamForChat.writeUTF("chat "+User.loggedInUser.getNickname()+": "+message.getText()+"\nreplied to "+
+            dataOutputStreamForChat.writeUTF("chat "+"image: "+User.loggedInUser.getProfileImageString() +
+                    " message: "+User.loggedInUser.getNickname()+": "+message.getText()+"\nreplied to "+
                     lastReplyMessage);
         }
         cancelReply((MouseEvent) event);
@@ -113,14 +120,73 @@ public class ChatRoomController  extends MenuController implements Initializable
         chatThread =new Thread(() -> {
         while (true) {
             try {
-                String inputMessage = dataInputStreamForChat.readUTF();
-                if (inputMessage.equals(User.loggedInUser.getUsername() + " gomsho")) {
+                String inputMessageInfo = dataInputStreamForChat.readUTF();
+                if (inputMessageInfo.equals(User.loggedInUser.getUsername() + " gomsho")) {
                     isChatEnded=true;
                     return;
                 }
-                if (!inputMessage.equals("")&&hasEnteredChatMenu) {
+                if (!inputMessageInfo.equals("")&&hasEnteredChatMenu) {
+                    Matcher matcher=getCommandMatcher(inputMessageInfo,"image: (.*) message: ([\\w\\d\\s\\n:,.?!]+)");
+                    String inputMessage=matcher.group(2);
+                    String imageAddress="src/resources"+matcher.group(1);
                     Platform.runLater(() -> {
                         if(!inputMessage.startsWith(User.loggedInUser.getNickname())){
+                            AnchorPane anchorPane=(AnchorPane) chatBox.getContent();
+                            double yLastMessage;
+                            if(anchorPane.getChildren().size()>0) {
+                                Group lastMessage = (Group) anchorPane.getChildren().get(anchorPane.getChildren().size()-1);
+                                yLastMessage = lastMessage.getLayoutY()+((Label)lastMessage.getChildren().get(1)).getHeight();
+                            }
+                            else {
+                                yLastMessage = -10;
+                            }
+                            Label messageLabel=new Label(inputMessage);
+                            Rectangle rectangle=new Rectangle(30,30);
+                            rectangle.setFill(new ImagePattern(new Image(new File(imageAddress).toURI().toString())));
+                            rectangle.setLayoutX(400);
+                            rectangle.setLayoutY(yLastMessage+10);
+                            Group message=new Group();
+                            message.setLayoutY(yLastMessage+5);
+                            message.getChildren().add(rectangle);
+                            message.getChildren().add(messageLabel);
+                            message.setOnDragDetected(new EventHandler<MouseEvent>() {
+                                @Override
+                                public void handle(MouseEvent mouseEvent) {
+                                    TranslateTransition translateTransition=new TranslateTransition();
+                                    translateTransition.setByX(-100);
+                                    translateTransition.setNode(message);
+                                    translateTransition.setDuration(Duration.seconds(0.5));
+                                    translateTransition.play();
+                                    translateTransition.setOnFinished(new EventHandler<ActionEvent>() {
+                                        @Override
+                                        public void handle(ActionEvent actionEvent) {
+                                            TranslateTransition translateTransition1=new TranslateTransition();
+                                            translateTransition1.setByX(100);
+                                            translateTransition1.setNode(message);
+                                            translateTransition1.setDuration(Duration.seconds(0.5));
+                                            translateTransition1.play();
+                                            translateTransition1.setOnFinished(new EventHandler<ActionEvent>() {
+                                                @Override
+                                                public void handle(ActionEvent actionEvent) {
+                                                    replyMessage.setText(messageLabel.getText());
+                                                    replyMessage.setTextFill(Color.ORANGE);
+                                                    replyMessage.setBackground(new Background(new BackgroundFill(Color.CYAN,CornerRadii.EMPTY,null)));
+                                                    cancelReply.setDisable(false);
+                                                    cancelReply.setOpacity(1);
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                            messageLabel.setTextFill(Color.CHARTREUSE);
+                            messageLabel.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY,null)));
+                            messageLabel.setLayoutY(yLastMessage+40);
+                            messageLabel.setOnMouseClicked(this::handleOthersMessages);
+                            messageLabel.setLayoutX(400);
+                            anchorPane.getChildren().add(message);
+                            chatBox.setContent(anchorPane);
+                            /*
                             AnchorPane anchorPane=(AnchorPane) chatBox.getContent();
                             double yLastMessage;
                             if(anchorPane.getChildren().size()>0) {
@@ -162,28 +228,37 @@ public class ChatRoomController  extends MenuController implements Initializable
                             });
                             messageLabel.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY,null)));
                             messageLabel.setTextFill(Color.CHARTREUSE);
+                            messageLabel.setOnMouseClicked(this::handleOthersMessages);
                             messageLabel.setLayoutY(yLastMessage+20);
                             messageLabel.setLayoutX(400);
                             anchorPane.getChildren().add(messageLabel);
-                            chatBox.setContent(anchorPane);
+                            chatBox.setContent(anchorPane);*/
                         }
                         else {
                             AnchorPane anchorPane=(AnchorPane) chatBox.getContent();
                             double yLastMessage;
                             if(anchorPane.getChildren().size()>0) {
-                                Label lastMessage = (Label) anchorPane.getChildren().get(anchorPane.getChildren().size()-1);
-                                yLastMessage = lastMessage.getLayoutY()+lastMessage.getHeight();
+                                Group lastMessage = (Group) anchorPane.getChildren().get(anchorPane.getChildren().size()-1);
+                               yLastMessage = lastMessage.getLayoutY()+((Label)lastMessage.getChildren().get(1)).getHeight();
                             }
                             else {
                                 yLastMessage = -10;
                             }
                             Label messageLabel=new Label(inputMessage);
-                            messageLabel.setOnDragDetected(new EventHandler<MouseEvent>() {
+                            Rectangle rectangle=new Rectangle(30,30);
+                            rectangle.setFill(new ImagePattern(new Image(new File(imageAddress).toURI().toString())));
+                            rectangle.setLayoutX(0);
+                            rectangle.setLayoutY(yLastMessage+10);
+                            Group message=new Group();
+                            message.setLayoutY(yLastMessage+5);
+                            message.getChildren().add(rectangle);
+                            message.getChildren().add(messageLabel);
+                            message.setOnDragDetected(new EventHandler<MouseEvent>() {
                                 @Override
                                 public void handle(MouseEvent mouseEvent) {
                                     TranslateTransition translateTransition=new TranslateTransition();
                                     translateTransition.setByX(100);
-                                    translateTransition.setNode(messageLabel);
+                                    translateTransition.setNode(message);
                                     translateTransition.setDuration(Duration.seconds(0.5));
                                     translateTransition.play();
                                     translateTransition.setOnFinished(new EventHandler<ActionEvent>() {
@@ -191,13 +266,14 @@ public class ChatRoomController  extends MenuController implements Initializable
                                         public void handle(ActionEvent actionEvent) {
                                             TranslateTransition translateTransition1=new TranslateTransition();
                                             translateTransition1.setToX(0);
-                                            translateTransition1.setNode(messageLabel);
+                                            translateTransition1.setNode(message);
                                             translateTransition1.setDuration(Duration.seconds(0.5));
                                             translateTransition1.play();
                                             translateTransition1.setOnFinished(new EventHandler<ActionEvent>() {
                                                 @Override
                                                 public void handle(ActionEvent actionEvent) {
                                                     replyMessage.setText(messageLabel.getText());
+                                                    replyMessage.setTextFill(Color.ORANGE);
                                                     replyMessage.setBackground(new Background(new BackgroundFill(Color.CYAN,CornerRadii.EMPTY,null)));
                                                     cancelReply.setDisable(false);
                                                     cancelReply.setOpacity(1);
@@ -209,10 +285,10 @@ public class ChatRoomController  extends MenuController implements Initializable
                             });
                             messageLabel.setTextFill(Color.RED);
                             messageLabel.setBackground(new Background(new BackgroundFill(Color.CHARTREUSE, CornerRadii.EMPTY,null)));
-                            messageLabel.setLayoutY(yLastMessage+20);
-                            System.out.println(yLastMessage+20);
+                            messageLabel.setLayoutY(yLastMessage+40);
+                            messageLabel.setOnMouseClicked(this::handleMyMessages);
                             messageLabel.setLayoutX(0);
-                            anchorPane.getChildren().add(messageLabel);
+                            anchorPane.getChildren().add(message);
                             chatBox.setContent(anchorPane);
                         }
                     });
@@ -225,9 +301,22 @@ public class ChatRoomController  extends MenuController implements Initializable
         chatThread.start();
     }
 
+    private void handleMyMessages(MouseEvent mouseEvent) {
+        if(mouseEvent.getButton()==MouseButton.SECONDARY){
+
+        }
+    }
+
+    private void handleOthersMessages(MouseEvent mouseEvent) {
+        if(mouseEvent.getButton()==MouseButton.SECONDARY){
+
+        }
+    }
+
     public void back(MouseEvent mouseEvent) throws Exception{
         dataOutputStreamForChat.writeUTF(User.loggedInUser.getUsername() + " exited Chatroom");
         dataOutputStreamForChat.flush();
+        hasEnteredChatMenu=false;
         SoundPlayable.playButtonSound("backButton");
         chatThread.stop();
         ChatRoom.close();
@@ -262,5 +351,13 @@ public class ChatRoomController  extends MenuController implements Initializable
         replyMessage.setBackground(null);
         cancelReply.setDisable(true);
         cancelReply.setOpacity(0.2);
+    }
+
+    public void setHoveredImageBackForReply(MouseEvent mouseEvent) {
+        cancelReply.setImage(new Image(new File("src/resources/yugioh/PNG/icon/closeHover.png").toURI().toString()));
+    }
+
+    public void resetHoveredImageBackForReply(MouseEvent mouseEvent) {
+        cancelReply.setImage(new Image(new File("src/resources/yugioh/PNG/icon/close.png").toURI().toString()));
     }
 }
